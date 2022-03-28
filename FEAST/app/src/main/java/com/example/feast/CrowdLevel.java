f1b3754b3e45001b8e84f3bace3a6853339a5196
skerbos <79687493+skerbos@users.noreply.github.com>
2022-03-29 03:10:21 +0800

    Addition of Weekly Tracker and changes
    Change log:
    - Added Weekly Tracker, extends Crowd Level, contains subclass DailyTracker (Despite the name it tracks the hourly highest capacity)
    - FNBEstablishment now implements Comparable, compares based on whether FNB isFavourite (boolean) by user and name (String) of FNB
    - CrowdLevel now implements method refreshInfo, updates currentCapacity by taking DataSnapshot from firebase and referencing a child with the name of FNB. Also checks and updates hourlyHighestCapacity if new currentCapacity is greater than previous hourlyHighestCapacity
    - Changed ExampleUnitTest class to FNBEstablishmentUnitTest for Javier's test cases for FNB
    - Added WeeklyTrackerTestUnit class
    
    NOTES:
    - There are issues with WeeklyTracker, briefly the test cases throws a StackOverflowError, I tried following the debug log but cannot really figure out what is wrong, need help
    - CrowdLevel's refreshInfo method has not been tested yet and is not completely modular for now, changes needed once merged with Front End

--- FEAST/app/src/main/java/com/example/feast/CrowdLevel.java
+++ FEAST/app/src/main/java/com/example/feast/CrowdLevel.java
@@ -1,32 +1,35 @@
 package com.example.feast;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 
+import java.time.*;
 import java.util.HashMap;
 
 public class CrowdLevel extends FNBEstablishment{
 
     static final String[] crowdLevelsString = {"Not Crowded", "A Little Crowded", "Somewhat Crowded", "Very Crowded", "Full"};
-    static final Double[] crowdLevelsDouble = {0.2, 0.5, 0.8, 1.0};
+    static final Double[] crowdLevelsThresholdDouble = {0.2, 0.5, 0.8, 1.0};
 
-    double currentCapacity;
-    double hourlyHighestCapacity;
+    int currentCapacity;
     double crowdPercentage;
     String currentCrowdLevel;
+    HashMap<Integer, Integer> hourlyHighestCapacity;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     CrowdLevel()
     {
         this.currentCapacity = 1;
-        this.hourlyHighestCapacity = 1;
         this.crowdPercentage = 1;
         this.currentCrowdLevel = "Not Crowded";
-    }
-
-    CrowdLevel(int currentCapacity, int hourlyHighestCapacity, double crowdPercentage, String currentCrowdLevel)
-    {
-        this.currentCapacity = 1;
-        this.hourlyHighestCapacity = 1;
-        this.crowdPercentage = 1;
+        this.hourlyHighestCapacity = new HashMap<>();
+        {
+            hourlyHighestCapacity.put(LocalTime.now().getHour(), this.currentCapacity);
+        }
     }
 
     public double getCurrentCapacity()
@@ -34,7 +37,7 @@ public class CrowdLevel extends FNBEstablishment{
         return this.currentCapacity;
     }
 
-    public double getHourlyHighestCapacity()
+    public HashMap<Integer, Integer> getHourlyHighestCapacity()
     {
         return this.hourlyHighestCapacity;
     }
@@ -45,27 +48,36 @@ public class CrowdLevel extends FNBEstablishment{
         return this.crowdPercentage;
     }
 
-    public void refreshInfo(DatabaseReference db)
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void refreshInfo(DataSnapshot ds)
     {
         //Get value of current capacity of store from database
+        this.currentCapacity = ds.child("people_count").child(super.name).child("currentCapacity").getValue(Integer.class);
         //Get value of hourly highest capacity of store from database
+        if (hourlyHighestCapacity.containsKey(LocalTime.now().getHour()))
+        {
+            if (hourlyHighestCapacity.get(LocalTime.now().getHour()) < this.currentCapacity)
+            {
+                hourlyHighestCapacity.put(LocalTime.now().getHour(), this.currentCapacity);
+            }
+        }
     }
 
     public String getCurrentCrowdLevel()
     {
-        if (this.calculateCrowdPercentage() <= crowdLevelsDouble[0])
+        if (this.calculateCrowdPercentage() <= crowdLevelsThresholdDouble[0])
         {
             return crowdLevelsString[0];
         }
-        else if (this.calculateCrowdPercentage() > crowdLevelsDouble[0] && this.calculateCrowdPercentage() <= crowdLevelsDouble[1])
+        else if (this.calculateCrowdPercentage() > crowdLevelsThresholdDouble[0] && this.calculateCrowdPercentage() <= crowdLevelsThresholdDouble[1])
         {
             return crowdLevelsString[1];
         }
-        else if (this.calculateCrowdPercentage() > crowdLevelsDouble[1] && this.calculateCrowdPercentage() <= crowdLevelsDouble[2])
+        else if (this.calculateCrowdPercentage() > crowdLevelsThresholdDouble[1] && this.calculateCrowdPercentage() <= crowdLevelsThresholdDouble[2])
         {
             return crowdLevelsString[2];
         }
-        else if (this.calculateCrowdPercentage() > crowdLevelsDouble[2] && this.calculateCrowdPercentage() <= crowdLevelsDouble[3])
+        else if (this.calculateCrowdPercentage() > crowdLevelsThresholdDouble[2] && this.calculateCrowdPercentage() <= crowdLevelsThresholdDouble[3])
         {
             return crowdLevelsString[3];
         }
@@ -74,6 +86,5 @@ public class CrowdLevel extends FNBEstablishment{
             return crowdLevelsString[4];
         }
     }
-
 
 }
