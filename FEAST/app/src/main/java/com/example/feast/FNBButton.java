fdfe778e7bafd022ad731f5bd3d3461efab53556
Fe-56 <limfuoen1999@gmail.com>
2022-04-06 19:54:40 +0800

    Update HomePage.java, FNBButton.java, fnb_button.xml, strings.xml,
    - Updated the code for the intents in HomePage.java
    - Also put in some example code to show how to use the widget setter methods in the FNBButton class
    - Added widget setter methods in the FNBButton class
    - Minor changes to strings.xml
    - Minor changes to fnb_button.xml to accommodate the slightly changed UI (where there will no long be opening hours shown in the MainPage)
    - Added a few more image resource files to res/drawables

--- FEAST/app/src/main/java/com/example/feast/FNBButton.java
+++ FEAST/app/src/main/java/com/example/feast/FNBButton.java
@@ -1,44 +1,57 @@
 package com.example.feast;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.os.Build;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.ColorRes;
+import androidx.annotation.RequiresApi;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import java.util.ArrayList;
 
-public class FNBButton extends ConstraintLayout {
+public class FNBButton extends ConstraintLayout{
     // all the variable data fields/images will be here as attributes of the FNBButton class
     private TextView fnbEstablishmentName; // for the name of the F&B establishment (e.g. Canteen)
+    private String fnbEstablishmentNameString; // for the Parcelable
     private TextView waitingTime; // for the estimated waiting time at the F&B establishment given current crowd levels
+    private String waitingTimeString; // for the Parcelable
     private TextView capacity; // for the current capacity of the F&B establishment, consisting of a word description and a percentage (e.g. Full 100%)
-    private TextView open; // for the opening hours of the F&B establishment (e.g. 8am to 7.30pm)
+    private String capacityString; // for the Parcelable
+    private TextView isOpen; // to indicate whether the F&B establishment is currently open or closed
+    private String isOpenString; // for the Parcelable
     private ImageView fnbPhoto; // for a picture of the F&B establishment, to grab the image from the database or from some website? Backend team to clarify
     private TextView dotText; // constant
-    private TextView openingHoursText; // constant
     private ImageView historicalTrendPhoto; // constant
     private TextView historyText; // constant
     private ImageView waitingTimePhoto; // constant
+    private Button favouriteIndicator; // to indicate whether the F&B establishment is a user's favourite
+    private boolean isFavourite; // whether this particular F&B establishment is in the favourites or not
     private ConstraintLayout fnbButton; // constant
+    private Context context;
 
     public FNBButton(Context context){ // constructor for FNBButton class
         super(context);
+        this.context = context;
         View view = LayoutInflater.from(getContext()).inflate(R.layout.fnb_button, null); // custom layout.xml file for the FNBButton
         this.fnbEstablishmentName = view.findViewById(R.id.fnbEstablishmentName);
         this.waitingTime = view.findViewById(R.id.waitingTime);
         this.capacity = view.findViewById(R.id.capacity);
-        this.open = view.findViewById(R.id.isOpen);
+        this.isOpen = view.findViewById(R.id.isOpen);
         this.fnbPhoto = view.findViewById(R.id.fnbPhoto);
         this.dotText = view.findViewById(R.id.dotText);
-        this.openingHoursText = view.findViewById(R.id.openingHoursText);
         this.historicalTrendPhoto = view.findViewById(R.id.historicalTrendPhoto);
         this.historyText = view.findViewById(R.id.historyText);
         this.waitingTimePhoto = view.findViewById(R.id.waitingTimePhoto);
+        this.favouriteIndicator = view.findViewById(R.id.favouriteIndicator);
         this.fnbButton = view.findViewById(R.id.fnbButton);
         this.addView(view);
     }
@@ -55,8 +68,8 @@ public class FNBButton extends ConstraintLayout {
         return capacity;
     }
 
-    public TextView getOpen() {
-        return open;
+    public TextView getIsOpen() {
+        return isOpen;
     }
 
     public ImageView getFnbPhoto() {
@@ -65,10 +78,6 @@ public class FNBButton extends ConstraintLayout {
 
     public TextView getDotText() {
         return dotText;
-    }
-
-    public TextView getOpeningHoursText() {
-        return openingHoursText;
     }
 
     public ImageView getHistoricalTrendPhoto() {
@@ -87,34 +96,153 @@ public class FNBButton extends ConstraintLayout {
         return fnbButton;
     }
 
-    // TODO
-    public void setFNBEstablishmentName(String value){
-//        this.fnbEstablishmentName.setText(R.string.test_fnbEstablishmentName); // pull the F&B establishment name from the database; currently a placeholder test value
-        this.fnbEstablishmentName.setText(value);
+    public String getFnbEstablishmentNameString() {
+        return fnbEstablishmentNameString;
     }
 
-    public void setWaitingTime(String value){
-//        this.waitingTime.setText(R.string.test_waitingTime); // pull the estimated waiting time at the F&B establishment from the database; currently a placeholder test value
-        this.waitingTime.setText(value);
+    public String getWaitingTimeString() {
+        return waitingTimeString;
     }
 
-    public void setCapacity(String value) {
-//        this.capacity.setText(R.string.test_capacity); // pull the current capacity of the F&B establishment from the database; currently a placeholder test value
-        this.capacity.setText(value);
+    public String getCapacityString() {
+        return capacityString;
     }
 
-    public void setOpen(String value){
-//        this.openingHours.setText(R.string.test_openingHours); // pull the current opening hours of the F&B establishment from the database; currently a placeholder test value
-        if (value == "Closed"){
-            this.open.setTextColor(Color.parseColor("#FF0000"));
+    public String getIsOpenString() {
+        return isOpenString;
+    }
+
+    public boolean getIsFavourite() {
+        return isFavourite;
+    }
+
+    public void setFNBEstablishmentName(String name){
+        this.fnbEstablishmentName.setText(name);
+        this.fnbEstablishmentNameString = name;
+
+        // this function will simultaneously set the FNBPhoto since it is dependant on the name of the FNB Establishment
+        switch(name){
+            case "Canteen":
+                this.fnbPhoto.setImageResource(R.drawable.canteen);
+                break;
+
+            case "D'Star Bistro":
+                this.fnbPhoto.setImageResource(R.drawable.d_star_bistro);
+                break;
+
+            case "Crooked Cooks":
+                this.fnbPhoto.setImageResource(R.drawable.crooked_cooks);
+                break;
+
+            case "Gomgom":
+                this.fnbPhoto.setImageResource(R.drawable.gomgom);
+                break;
+
+            case "Simon's":
+                this.fnbPhoto.setImageResource(R.drawable.simon);
+                break;
         }
+    }
+
+    public void setWaitingTime(String waitingTime){
+        this.waitingTime.setText(waitingTime);
+        this.waitingTimeString = waitingTime;
+    }
+
+    public void setCapacity(String crowdLevel, double crowdPercentage) {
+        switch(crowdLevel){
+            case "Not Crowded":
+                this.capacity.setTextColor(Color.parseColor("#008000")); // sets the color of the capacity TextView to Green (#008000)
+                break;
+
+            case "Crowded":
+                this.capacity.setTextColor(Color.parseColor("#FFA500")); // sets the color of the capacity TextView to Orange (#FFA500)
+                break;
+
+            case "Very Crowded":
+                this.capacity.setTextColor(Color.parseColor("#FF8C00")); // sets the color of the capacity TextView to Dark Orange (#FF8C00)
+                break;
+
+            case "Full":
+                this.capacity.setTextColor(Color.parseColor("#FF0000")); // sets the color of the capacity TextView to Red (#FF0000)
+                break;
+        }
+
+        String tempCapacity = crowdLevel;
+        double rawCrowdPercentage = crowdPercentage * 100;
+        int simplifiedCrowdPercentage = (int) rawCrowdPercentage;
+        tempCapacity += " " + String.valueOf(simplifiedCrowdPercentage) + "%"; // sets the percentage of the crowd level
+        this.capacity.setText(tempCapacity);
+        this.capacityString = tempCapacity;
+    }
+
+    public void setIsOpen(String openStatus){
+        if (openStatus.equals("Closed")){
+            this.isOpen.setTextColor(Color.parseColor("#FF0000")); // if the fnb establishment is closed, change the text colour to Red (#FF0000)
+        }
+
         else{
-            this.open.setTextColor(Color.parseColor("#008000"));
+            this.isOpen.setTextColor(Color.parseColor("#008000")); // if the fnb establishment is open, change the text colour to Green (#008000)
         }
-        this.open.setText(value);
+
+        this.isOpen.setText(openStatus);
+        this.isOpenString = openStatus;
     }
 
-    public void setFNBPhoto(String value){
-        this.fnbPhoto.setImageResource(R.drawable.test_sutd_canteen); // pull the photo of the F&B establishment from the database; currently a placeholder test image
+    public void setIsFavourite(boolean favouriteOrNot){
+        if (favouriteOrNot){ // if the input value is true, meaning that this particular F&B establishment is in the user's favourites list
+            this.favouriteIndicator.setBackgroundColor(Color.parseColor("#FFD700")); // sets it to gold colour (#FFD700)
+        }
+
+        else{
+            this.favouriteIndicator.setBackgroundColor(Color.parseColor("#FFFFFF")); // sets it to white colour (#FFFFFF)
+        }
+
+        this.isFavourite = favouriteOrNot;
     }
+
+    // to make the FNBButton object be able to be passed into the implicit intents
+    // ignore for now, can leave this as a legacy or something lol - Fuo En
+//    @RequiresApi(api = Build.VERSION_CODES.Q)
+//    @Override
+//    public void writeToParcel(Parcel out, int flags){
+//        out.writeString(this.fnbEstablishmentName.getText().toString());
+//        out.writeString(this.waitingTime.getText().toString());
+//        out.writeString(this.capacity.getText().toString());
+//        out.writeString(this.isOpen.getText().toString());
+//        out.writeBoolean(this.isFavourite);
+//    }
+//
+//    @RequiresApi(api = Build.VERSION_CODES.Q)
+//    private void readFromParcel(Parcel in){
+//        this.fnbEstablishmentNameString = in.readString();
+//        this.waitingTimeString = in.readString();
+//        this.capacityString = in.readString();
+//        this.isOpenString = in.readString();
+//        this.isFavourite = in.readBoolean();
+//    }
+//
+//    @RequiresApi(api = Build.VERSION_CODES.Q)
+//    public FNBButton(Parcel in, Context context){
+//        super(context);
+//        readFromParcel(in);
+//    }
+//
+//    @Override
+//    public int describeContents() {
+//        return 0;
+//    }
+//
+//    public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+//        Context context = this.context;
+//
+//        @RequiresApi(api = Build.VERSION_CODES.Q)
+//        public FNBButton createFromParcel(Parcel in){
+//            return new FNBButton(in, this.context);
+//        }
+//
+//        public FNBButton[] newArray(int size){
+//            return new FNBButton[size];
+//        }
+//    };
 }
